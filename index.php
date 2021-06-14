5880c099175cc93aa8f6c510f1a8040c3111861f
Goran <goran.schyns@student.hepl.be>
2021-06-14 13:58:38 +0200

    work in progress

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+require('./configs/config.php');
+require('./utils/dbaccess.php');
+//require('./models/user.php');
+
+$connection = getConnection();
+
+echo 'Test';
+
+/*
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $user = find($connection, $id);
+    $view = 'user.php';
+} else {
+    $users = all($connection);
+    $view = 'users.php';
+}
+
+require("./views/{$view}");
+*/
